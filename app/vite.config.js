8cb6ae50845cdfa83cef1a96a649c660443ca6c3
Harshdeep <kinngh@me.com>
2025-05-03 18:21:49 +0530

    Remove the custom vite plugin

--- app/vite.config.js
+++ app/vite.config.js
@@ -2,11 +2,10 @@ import react from "@vitejs/plugin-react";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
-import routePlugin from "./src/vite-plugin-routes";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), routePlugin()],
+  plugins: [react()],
   root: dirname(fileURLToPath(import.meta.url)),
   resolve: {
     preserveSymlinks: true,
